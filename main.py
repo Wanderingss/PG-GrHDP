1c6d133650d8df6099d54d4cb472ee3e02ee43ca
Wanderingss <49053421+Wanderingss@users.noreply.github.com>
2022-03-31 20:53:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ from network import Critic, Actor
 
 
 if __name__ == '__main__':
-    eng = matlab.engine.start_matlab()
+   
 
     # 导入训练好的模型
-    TestPolicy.Model.Player = torch.load('./ModelData/Player.ckpt')
-    TestPolicy.Model.Signal = torch.load('./ModelData/Signal.ckpt')
-    TestPolicy.Model.ValueFunction = torch.load('./ModelData/ValueFunction.ckpt')
+    TestPolicy = ValueIterative()
+    TestPolicy.Model.Player = torch.load('Player.ckpt')
+    TestPolicy.Model.Signal = torch.load('Signal.ckpt')
+    TestPolicy.Model.ValueFunction = torch.load('ValueFunction.ckpt')
 
     # 查看训练好的模型响应曲线
     State = torch.tensor([[0.6,-0.6]])
     TestPolicy.Model.beta = 0.01
-    print(1/(1-TestPolicy.Model.beta))
     TestPolicy.EndTime = 60
     TestPolicy.ViewDynamic(controller="event-triggered", State=State) # 事件触发
-    #TestPolicy.ViewDynamic(controller="neural", State=State)         # 时间触发
+    TestPolicy.ViewDynamic(controller="neural", State=State)         # 时间触发
 
     #训练模型
     '''
+    print("Training start")
+    eng = matlab.engine.start_matlab()
     for i in range(20):
         # 产生训练数据 不需要系统模型
         TrainReinforcement_signalData,TrainLoss  = TestPolicy.GetTrainData()
@@ -30,12 +32,12 @@ if __name__ == '__main__':
         TrainLoss[:,3]= TrainLoss[:,3] - TestPolicy.Model.ValueFunction(torch.tensor([[0.0,0.0,0.0]])).detach().numpy()
         
         # 训练目标网络
-        accuracy = TestPolicy.MatlabTrainValueFunction(eng, TestPolicy.Model.Signal, TrainReinforcement_signalData, Minaccuracy=0.00000001, Maxepochs=100, MaxAccuracy=0.01, showWindow=1)
+        accuracy = TestPolicy.MatlabTrainValueFunction(eng, TestPolicy.Model.Signal, TrainReinforcement_signalData, Minaccuracy=0.00000001, Maxepochs=100, MaxAccuracy=0.01, showWindow=0)
         print(accuracy)
         torch.save(TestPolicy.Model.Signal, 'Signal.ckpt')
         
         # 训练值网络
-        accuracy = TestPolicy.MatlabTrainValueFunction(eng, TestPolicy.Model.ValueFunction, TrainLoss, Minaccuracy=0.00000001, Maxepochs=100, MaxAccuracy=0.01, showWindow=1)
+        accuracy = TestPolicy.MatlabTrainValueFunction(eng, TestPolicy.Model.ValueFunction, TrainLoss, Minaccuracy=0.00000001, Maxepochs=100, MaxAccuracy=0.01, showWindow=0)
         print(accuracy)
         torch.save(TestPolicy.Model.ValueFunction, 'ValueFunction.ckpt')
         
